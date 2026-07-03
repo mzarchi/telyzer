921dd8541faa1231dc6975a1a0f6204b6c53d4a2
Mohammad Zarchi <mzarchi94@gmail.com>
2026-07-04 03:26:04 +0330

    Add main login for app

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from app.ui.login import LoginWindow
+from app.ui.login import LoginWindow, STYLESHEET
 
-app = QApplication(sys.argv)
+def main():
+    app = QApplication(sys.argv)
+    app.setStyleSheet(STYLESHEET)
+    
+    win = LoginWindow()
+    
+    screen = app.primaryScreen().geometry()
+    x = (screen.width() - win.width()) // 2
+    y = (screen.height() - win.height()) // 2
+    win.move(x, y)
+    
+    win.show()
+    sys.exit(app.exec())
 
-window = LoginWindow()
-window.show()
-
-sys.exit(app.exec())
+if __name__ == "__main__":
+    main()
