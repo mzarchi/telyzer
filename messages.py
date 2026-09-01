61f8fc1181b29348afbc3e40d77bf62ef19df045
Mohammad Zarchi <mzarchi94@gmail.com>
2026-09-01 10:43:30 +0330

    Create a message file fir clean code

---
+++ messages.py
@@ -0,0 +1,11 @@
+msg_main = """Please Select one of this option:
+    1. Telegram Login
+    2. Channel Analysis
+    4. Group Analysis
+    3. Chat Analysis
+    4. Export
+    5. Setting
+    6. About
+    e. Exit
+    
+Enter you choose: """
