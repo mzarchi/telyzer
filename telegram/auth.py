bf465fdb38dfc36850b02cb2d359d0eb020ed5af
Mohammad Zarchi <mzarchi94@gmail.com>
2026-09-02 16:04:06 +0330

    Create a Telegram Class and init

--- telegram/auth.py
+++ telegram/auth.py
@@ -2,5 +2,15 @@ from telethon import TelegramClient
 
 
 class TelegramAuth:
-    def __init__(self):
-        pass
+    def __init__(self, api_id, api_hash, session_path):
+        self.client = TelegramClient(
+            session_path,
+            api_id,
+            api_hash
+        )
+
+    async def login(self):
+        await self.client.start()
+        if await self.client.is_user_authorized():
+            return True
+        return False
