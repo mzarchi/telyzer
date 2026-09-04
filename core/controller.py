b9b5580d3fa6da2c10f77d607d8f4f4c41efe334
Mohammad Zarchi <mzarchi94@gmail.com>
2026-09-04 22:43:35 +0330

    Add @username to main menu

--- core/controller.py
+++ core/controller.py
@@ -13,14 +13,36 @@ class TelyzerController:
                 config.api_hash,
                 config.app_version
             )
-            
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(config.ta._create_client())
 
     def connect(self):
         is_valid = self.loop.run_until_complete(config.ta.connect())
         if not is_valid:
             self.loop.run_until_complete(config.ta.login())
+
+    def is_session_active(self):
+        if config.ta is None or config.ta.client is None:
+            return False
+        return self.loop.run_until_complete(config.ta.connect())
+
+    def get_me(self):
+        if config.ta is None or config.ta.client is None:
+            return None
+        return self.loop.run_until_complete(config.ta.client.get_me())
+
+    def logout(self):
+        submit = input("Are you sure you want to logout? (y/n): ")
+        if submit.lower() != "n":
+            async def _logout():
+                await config.ta.client.log_out()
+                print("Logged out!")
+                input("Press Enter to continue...")
+            self.loop.run_until_complete(_logout())
+        else:
+            print("Logout cancelled.")
+            input("Press Enter to continue...")
 
     def cls(self):
         os.system('cls' if os.name == 'nt' else 'clear')
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import messages as msg
 from core.controller import TelyzerController
+import config
 
 
 def main():
     tc = TelyzerController()
+    
     while True:
         tc.cls()
-        input_user_choose = input(msg.msg_main)
+        menu = msg.msg_main
+        if config.ta and config.ta.client:
+            is_active = tc.is_session_active()
+            if is_active:
+                me = tc.get_me()
+                if me:
+                    username = me.username or me.first_name
+                    menu = msg.msg_main.replace("1. Telegram Login", f"1. Session of @{username} is active - Logout")
+        
+        input_user_choose = input(menu)
         if input_user_choose == "e":
             break
-        else:
-            match input_user_choose:
-                case "1":
+        
+        match input_user_choose:
+            case "1":
+                if tc.is_session_active():
+                    tc.logout()
+                else:
                     tc.connect()
 
 
