76fecb45c8d66a1d4cd05f77751af080545eb698
Mohammad Zarchi <mzarchi94@gmail.com>
2026-09-04 21:00:48 +0330

    Create a _init class and login def

--- telegram/auth.py
+++ telegram/auth.py
@@ -3,28 +3,35 @@ from telethon.errors import SessionPasswordNeededError
 
 
 class TelegramAuth:
-    def __init__(self, api_id, api_hash, session_path):
-        self.client = TelegramClient(
-            session_path,
-            api_id,
-            api_hash
-        )
+    client = None
+
+    def __init__(self, session_path, api_id, api_hash):
+        self.session_path = session_path
+        self.api_id = api_id
+        self.api_hash = api_hash
 
     async def login(self):
+        self.client = TelegramClient(
+            self.session_path,
+            self.api_id,
+            self.api_hash
+        )
+
         await self.client.connect()
         if await self.client.is_user_authorized():
             print("Your session is active")
             return True
-        
+
         phone = input("Your Phone Number: ")
         await self.client.send_code_request(phone)
-        
+
         code = input("Enter Login Code: ")
         try:
             await self.client.sign_in(phone=phone, code=code)
-            
+
         except SessionPasswordNeededError as spne:
             c2fa = input("Enter 2FA password: ")
             await self.client.sign_in(password=c2fa)
-        print("Login seccessful")
+
+        print("Login successful")
         return True
